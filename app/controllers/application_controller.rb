42dbecef36e63aae1fd0c0e483f50ed5ac9e0b48
tomozou-miyake <tomoki2802@gmail.com>
2019-09-18 17:14:29 +0900

    Add production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
